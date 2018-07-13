6efd0ed721035c6817090e82a394b88fa75697b6
Ross Wendt <rosswendt@gmail.com>
2018-07-13 13:01:15 -0600

    Fixed ex-7.py (shap explainer example) to run with shap source code. Placed ex-7.py into new folder called ex-7-with-source, that also contains shap source code. Removed apple_strawberry and replaced with image_1 which is an image of a raspberry, blueberry, and strawberry on a small white plate, where the white plate is on a wooden table surface

--- book_code/ex-7-with-source/ex-7.py
+++ book_code/ex-7-with-source/ex-7.py
@@ -2,11 +2,18 @@
 
 Based on https://github.com/slundberg/shap'''
 
-from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.preprocessing import image
-import skimage
+import skimage, shap, numpy as np
+from skimage.segmentation import slic
 
-'''Defines an image mask.'''
+model = VGG16()
+file = "data/image_1.jpg"
+img = image.load_img(file, target_size=(224, 224))
+img_orig = image.img_to_array(img)
+#segments_slice = skimage.segmentation.slic(img, n_segments=50, compactness=30, sigma=3)
+segments_slice = slic(img, n_segments=50, compactness=30, sigma=3)
+
 def mask_image(zs, segmentation, image, background=None):
     if background is None:
         background = image.mean((0,1))
@@ -18,20 +25,16 @@ def mask_image(zs, segmentation, image, background=None):
                 out[i][segmentation == j,:] = background
     return out
 
-'Runs a prediction on the preprocessing masked image'
 def f(z):
-    return model.predict(preprocess_input(mask_image(z, segments_slic, img_orig, 255)))
+    return model.predict(preprocess_input(mask_image(z, segments_slice, img_orig, 255)))
 
-'Runs p. 217 Shapley code.'
 def main():
-    model = VGG16()
-    file = "data/apple_strawberry.jpg"
-    img = image.load_img(file, target_size=(224, 224))
-    img_orig = image.img_to_array(img)
-    segments_slice = skimage.segmentation.slic(img, n_segments=50, compactness=30, sigma=3)
     explainer = shap.KernelExplainer(f, np.zeros((1,50)))
     shap_values = explainer.shap_values(np.ones((1,50)), nsamples=1000)
     preds = model.predict(preprocess_input(np.expand_dims(img_orig.copy(), axis=0)))
     top_preds = np.argsort(-preds)
+
+    shap.
+    #shap.image_plot(shape_values, preds)
 
 main()
